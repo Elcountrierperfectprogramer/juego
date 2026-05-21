004423264e7106bea9908adfde6d5c029e472b91
Rodrigo-173465 <rodrigogiltorres73@gmail.com>
2026-05-21 14:52:18 -0400

    Update menu.js

--- juego3d_con_rod/menu.js
+++ juego3d_con_rod/menu.js
@@ -1,1 +1,10 @@
+// menu.js
 
+// Crear menú
+function menu() {
+imageObj = new Image();
+imageObj.src = "https://raw.githubusercontent.com/Elcountrierperfectprogramer/juego/refs/heads/main/juego3d_con_rod/menu.jpg";
+ctx.drawImage(imageObj, 100, 100);
+}
+
+menu();
