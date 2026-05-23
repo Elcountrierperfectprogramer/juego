eefa015e061ac660c068f9f97d2550faf45d0e1b
elcountrierperfectprogramer <aldoreich4ver@gmail.com>
2026-05-23 19:07:15 -0400

    Update script.js
    Locura

--- juego3d/script.js
+++ juego3d/script.js
@@ -193,8 +193,8 @@ camera.lookAt(0, 0, 0);
 function animate() {
     cameraGroup.rotation.y = -cameraRotationY;
     camera.rotation.x = -cameraRotationX;
-    cameraGroup.position.y -= 10;
-    camera.position.y += joydeltaY;
+    cameraGroup.position.y -= 100;
+    camera.position.z += joydeltaY;
     camera.position.x += joydeltaX;
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
